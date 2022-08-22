2e590ea0305c97afd3d0905628f6853dfe9cb3e2
failamir <fail.amir@students.amikom.ac.id>
2022-08-23 01:31:35 +0700

    $_SERVER['HTTP_REFERER']

--- modules/Job/Views/frontend/layouts/detail-ver/job-single-v2.blade.php
+++ modules/Job/Views/frontend/layouts/detail-ver/job-single-v2.blade.php
@@ -20,7 +20,7 @@
 
                     @include("Job::frontend.layouts.details.video")
 
-                    <a href="/page/recruitment-procedure-job" class="theme-btn btn-style-two">Recruitment Procudure</a>
+                    <a href="/redirect.php" class="theme-btn btn-style-two">Recruitment Procudure</a>
 
                     @include("Job::frontend.layouts.details.social-share")
 
