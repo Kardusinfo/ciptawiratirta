5ae0bb963cb53a61c222b9e55f8f5cf669916a55
failamir <fail.amir@students.amikom.ac.id>
2022-08-23 12:58:57 +0700

    modal-custom

--- modules/Template/Views/frontend/blocks/hero-banner/style_9.blade.php
+++ modules/Template/Views/frontend/blocks/hero-banner/style_9.blade.php
@@ -36,7 +36,7 @@
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        width: 600px;
+        width: 500px;
         max-width: 100%;
         height: auto;
         max-height: 80%;
