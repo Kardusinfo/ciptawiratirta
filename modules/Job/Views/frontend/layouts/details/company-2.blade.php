dde92682322743ed50f5ba3c8490e100694f4698
failamir <fail.amir@students.amikom.ac.id>
2022-08-24 18:08:59 +0700

    Update company-2.blade.php

--- modules/Job/Views/frontend/layouts/details/company-2.blade.php
+++ modules/Job/Views/frontend/layouts/details/company-2.blade.php
@@ -5,11 +5,13 @@
             <div class="company-title">
                 @if(!empty($row->company->avatar_id))
                     <div class="company-logo">
-                        <a href="{{ $row->company->getDetailUrl() }}"><img src="{{ \Modules\Media\Helpers\FileHelper::url($row->company->avatar_id) }}" alt="{{ $company_translation->name }}"></a>
+                        {{-- <a href="{{ $row->company->getDetailUrl() }}"><img src="{{ \Modules\Media\Helpers\FileHelper::url($row->company->avatar_id) }}" alt="{{ $company_translation->name }}"></a> --}}
+                        <a href="{{ $row->company->website }}"><img src="{{ \Modules\Media\Helpers\FileHelper::url($row->company->avatar_id) }}" alt="{{ $company_translation->name }}"></a>
                     </div>
                 @endif
-                <h5 class="company-name"><a style="color:inherit" href="{{ $row->company->getDetailUrl() }}">{{ $company_translation->name }}</a></h5>
-                <a href="{{ $row->company->getDetailUrl() }}" class="profile-link">{{ __("View company profile") }}</a>
+                {{-- <h5 class="company-name"><a style="color:inherit" href="{{ $row->company->getDetailUrl() }}">{{ $company_translation->name }}</a></h5> --}}
+                <h5 class="company-name"><a style="color:inherit" href="{{ $row->company->website }}">{{ $company_translation->name }}</a></h5>
+                <a href="{{ $row->company->website }}" class="profile-link">{{ __("View company profile") }}</a>
             </div>
         </div>
     </div>
