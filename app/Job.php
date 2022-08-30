cc21a94b4e2ac4954cea121418536b8d65d0ca4b
failamir <fail.amir@students.amikom.ac.id>
2022-08-30 21:34:59 +0700

    qwerty

--- app/Job.php
+++ app/Job.php
@@ -1,9 +1,9 @@
 <?php
-namespace App\Models;
+namespace App;
 
 use Illuminate\Support\Facades\App;
 
-class Sgp extends \App\Sgp
+class Job extends \App\Models\Job
 {
     protected $dateFormat = 'Y-m-d H:i:sO';
 }
