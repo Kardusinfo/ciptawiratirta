7acb17474268b8ee69463a458febc1bd00bb19f6
fadjarrafi <dark.fir21@gmail.com>
2022-08-30 22:26:22 +0700

    Update show.blade.php
    fix layout show data users

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -16,6 +16,18 @@
             <table class="table table-bordered">
                 <tbody>
                     <tr>
+                        <th rowspan="5">
+                            {{ trans('cruds.user.fields.photo') }}
+                        </th>
+                        <td>
+                            @if($user->photo)
+                                <a href="{{ $user->photo->getUrl() }}" target="_blank" style="display: inline-block">
+                                    <img src="{{ $user->photo->getUrl('thumb') }}">
+                                </a>
+                            @endif
+                        </td>
+                    </tr>
+                    <tr>
                         <th>
                             {{ trans('cruds.user.fields.id') }}
                         </th>
@@ -281,18 +293,6 @@
                         </th>
                         <td>
                             {{ $user->contact_no }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.user.fields.photo') }}
-                        </th>
-                        <td>
-                            @if($user->photo)
-                                <a href="{{ $user->photo->getUrl() }}" target="_blank" style="display: inline-block">
-                                    <img src="{{ $user->photo->getUrl('thumb') }}">
-                                </a>
-                            @endif
                         </td>
                     </tr>
                 </tbody>
